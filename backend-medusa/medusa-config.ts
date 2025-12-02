6380de24029b3d202e4257c4e06bb1d1d693d989
Vosmozhno <sidorenodima09@gmail.com>
2025-12-02 16:36:04 +0300

    update config.ts

--- backend-medusa/medusa-config.ts
+++ backend-medusa/medusa-config.ts
@@ -5,12 +5,20 @@ loadEnv(process.env.NODE_ENV || 'development', process.cwd())
 module.exports = defineConfig({
   projectConfig: {
     databaseUrl: process.env.DATABASE_URL,
+    redisUrl: process.env.REDIS_URL,
+    
+    workerMode: process.env.MEDUSA_WORKER_MODE as "shared" | "worker" | "server" || "shared",
+    
     http: {
-      storeCors: process.env.STORE_CORS || "http://localhost:3000",
+      storeCors: process.env.STORE_CORS!,
       adminCors: process.env.ADMIN_CORS!,
-      authCors: process.env.AUTH_CORS || "http://localhost:3000",
+      authCors: process.env.AUTH_CORS!,
       jwtSecret: process.env.JWT_SECRET || "supersecret",
       cookieSecret: process.env.COOKIE_SECRET || "supersecret",
     }
+  },
+  admin: {
+    disable: process.env.DISABLE_MEDUSA_ADMIN === "true",
+    path: "/app", 
   }
 })
