52bef07fd474120571eee0a77e4b68bfd87c906b
Vosmozhno <sidorenodima09@gmail.com>
2025-12-02 17:59:11 +0300

    change admin path to dashboard and refresh dependencies

--- backend-medusa/medusa-config.ts
+++ backend-medusa/medusa-config.ts
@@ -7,18 +7,17 @@ module.exports = defineConfig({
     databaseUrl: process.env.DATABASE_URL,
     redisUrl: process.env.REDIS_URL,
     
-    workerMode: process.env.MEDUSA_WORKER_MODE as "shared" | "worker" | "server" || "shared",
-    
     http: {
       storeCors: process.env.STORE_CORS!,
       adminCors: process.env.ADMIN_CORS!,
       authCors: process.env.AUTH_CORS!,
       jwtSecret: process.env.JWT_SECRET || "supersecret",
       cookieSecret: process.env.COOKIE_SECRET || "supersecret",
-    }
+    },
+    workerMode: process.env.MEDUSA_WORKER_MODE as "shared" | "worker" | "server" || "shared",
   },
   admin: {
-    disable: process.env.DISABLE_MEDUSA_ADMIN === "true",
-    path: "/app", 
+    disable: false,
+    path: "/dashboard",
   }
 })
