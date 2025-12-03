c9d60b2dc501b2ef446a6f7b65e24c35dd2170b9
Vosmozhno <sidorenodima09@gmail.com>
2025-12-03 22:11:08 +0300

    configure S3

--- cms/config/plugins.ts
+++ cms/config/plugins.ts
@@ -1,1 +1,25 @@
-export default () => ({});
+export default ({ env }) => ({
+  upload: {
+    config: {
+      provider: 'aws-s3',
+      providerOptions: {
+        accessKeyId: env('B2_KEY_ID'),
+        secretAccessKey: env('B2_KEY_SECRET'),
+        region: env('B2_REGION'),
+        endpoint: env('B2_ENDPOINT'),
+        s3ForcePathStyle: true,
+        
+        baseUrl: env('B2_S3_URL'), 
+        
+        params: {
+          Bucket: env('B2_BUCKET'),
+        },
+      },
+      actionOptions: {
+        upload: {},
+        uploadStream: {},
+        delete: {},
+      },
+    },
+  },
+});
