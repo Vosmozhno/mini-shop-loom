2aa573c8e026531761b2db0c1233658f92b525f8
Vosmozhno <sidorenodima09@gmail.com>
2025-12-03 23:00:21 +0300

    change baseUrl to cdnBaseUrl

--- cms/config/plugins.ts
+++ cms/config/plugins.ts
@@ -1,24 +1,21 @@
 export default ({ env }) => ({
   upload: {
     config: {
-      provider: 'aws-s3',
+      provider: "aws-s3",
       providerOptions: {
         credentials: {
-          accessKeyId: env('B2_KEY_ID'),
-          secretAccessKey: env('B2_KEY_SECRET'),
+          accessKeyId: env("B2_KEY_ID"),
+          secretAccessKey: env("B2_KEY_SECRET"),
         },
-        region: env('B2_REGION'),
-        endpoint: env('B2_ENDPOINT'),
+        region: env("B2_REGION"),
+        endpoint: env("B2_ENDPOINT"),
         s3ForcePathStyle: true,
-        baseUrl: env('B2_S3_URL'),
+
+        cdnBaseUrl: env("B2_S3_URL"),
+
         params: {
-          Bucket: env('B2_BUCKET'),
+          Bucket: env("B2_BUCKET"),
         },
-      },
-      actionOptions: {
-        upload: {},
-        uploadStream: {},
-        delete: {},
       },
     },
   },
