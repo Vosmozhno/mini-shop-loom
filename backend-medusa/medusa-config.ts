fd7456a283580703d4734b7a78fd1173907e8b7e
Vosmozhno <sidorenodima09@gmail.com>
2025-12-03 11:24:52 +0300

    fix b2 config

--- backend-medusa/medusa-config.ts
+++ backend-medusa/medusa-config.ts
@@ -12,27 +12,32 @@ module.exports = defineConfig({
       authCors: process.env.AUTH_CORS!,
       jwtSecret: process.env.JWT_SECRET || "supersecret",
       cookieSecret: process.env.COOKIE_SECRET || "supersecret",
-      trustProxy: true, 
+      trustProxy: true,
     } as any,
-    
     workerMode: process.env.MEDUSA_WORKER_MODE as "shared" | "worker" | "server" || "shared",
   },
   admin: {
     path: "/",
     disable: !process.env.VERCEL,
   },
-  modules: {
-    file: {
-      resolve: "@medusajs/file-s3",
+  modules: [
+    {
+      resolve: "@medusajs/medusa/file", 
       options: {
-        s3_url: process.env.B2_S3_URL,
-        bucket: process.env.B2_BUCKET,
-        region: "us-east-1",
-        credentials: {
-          access_key_id: process.env.B2_KEY_ID,
-          secret_access_key: process.env.B2_KEY_SECRET,
-        },
+        providers: [
+          {
+            resolve: "@medusajs/file-s3", 
+            id: "s3",
+            options: {
+              file_url: process.env.B2_S3_URL,
+              bucket: process.env.B2_BUCKET,
+              region: "us-east-1",
+              access_key_id: process.env.B2_KEY_ID,
+              secret_access_key: process.env.B2_KEY_SECRET,
+            },
+          },
+        ],
       },
     },
-  },
+  ],
 })
