2830fefcfe230cde8442272019d1ecdcb1700805
Vosmozhno <sidorenodima09@gmail.com>
2025-12-03 22:37:48 +0300

    fix s3 plugin

--- cms/config/plugins.ts
+++ cms/config/plugins.ts
@@ -3,14 +3,14 @@ export default ({ env }) => ({
     config: {
       provider: 'aws-s3',
       providerOptions: {
-        accessKeyId: env('B2_KEY_ID'),
-        secretAccessKey: env('B2_KEY_SECRET'),
+        credentials: {
+          accessKeyId: env('B2_KEY_ID'),
+          secretAccessKey: env('B2_KEY_SECRET'),
+        },
         region: env('B2_REGION'),
         endpoint: env('B2_ENDPOINT'),
         s3ForcePathStyle: true,
-        
-        baseUrl: env('B2_S3_URL'), 
-        
+        baseUrl: env('B2_S3_URL'),
         params: {
           Bucket: env('B2_BUCKET'),
         },
